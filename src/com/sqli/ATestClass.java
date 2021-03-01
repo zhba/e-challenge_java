35f9a8a87d65eab721c6791457bbe3c540bf3b12
zhba <zhba@sqli.com>
2021-03-01 15:26:04 +0100

    Add ATestClass that displays a simple text

---
+++ src/com/sqli/ATestClass.java
@@ -0,0 +1,9 @@
+package com.sqli;
+
+public class ATestClass {
+
+    public void ATestMethod(String aTestParam){
+        System.out.println("Method says : " + aTestParam);
+    }
+
+}
--- src/com/sqli/Main.java
+++ src/com/sqli/Main.java
@@ -3,6 +3,7 @@ package com.sqli;
 public class Main {
 
     public static void main(String[] args) {
-
+        ATestClass testClass = new ATestClass();
+        testClass.ATestMethod("A sample text!");
     }
 }
